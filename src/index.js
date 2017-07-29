1bd0443beca4ca687eaf8267746bd1dc11bad66a
Kaj Ström <kaj.strom@kapsi.fi>
2017-07-29 15:17:59 +0300

    Brings in RxJS and implements event streams for weight and reperations.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+import Rx from "rxjs/Rx";
+
 require("bootstrap-loader");
 
-let stuff = "abc1122";
+const Observable = Rx.Observable;
+const weightInput = document.querySelector("#weight");
+const repetitionInput = document.querySelector("#repetitions");
 
-console.log(stuff);
+const weight = Observable.fromEvent(weightInput, "keyup")
+    .map(() => Number.parseInt(weightInput.value))
+    .filter((weight) => !Number.isNaN(weight));
+
+const repetitions = Observable.fromEvent(repetitionInput, "keyup")
+    .map(() => Number.parseInt(repetitionInput.value))
+    .filter((repetitions) => !Number.isNaN(repetitions));
+
+weight.subscribe((value) => {
+    console.log(value);
+});
+
+const calculations = weight.combineLatest(
+        repetitions,
+        (weight, repetitions) => {return {weight, repetitions}}
+    )
+    .distinctUntilChanged();
+
+calculations.subscribe(function (x) {
+    console.log(x);
+});
